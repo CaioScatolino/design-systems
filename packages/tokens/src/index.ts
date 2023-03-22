5d9355d37fedee227b6ee853e90f6f676b95c28f
Caio Scatolino <caio.scatolino@outlook.com>
2023-03-22 15:54:23 -0300

    exportando tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,13 @@
 export * from './colors';
+
+export * from './font-sizes';
+export * from './font-weights';
+export * from './fonts';
+export * from './line-heights';
+export * from './font-weights';
+
+
+export * from './radii'
+export * from './space'
+
+
