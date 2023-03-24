c489af6fd9ea1d6b01f847443cef64d159a04946
Caio Scatolino <caio.scatolino@outlook.com>
2023-03-24 15:52:30 -0300

    configurando eslint @rocketseat/eslint-config

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,13 +1,10 @@
-export * from './colors';
+export * from './colors'
 
-export * from './font-sizes';
-export * from './font-weights';
-export * from './fonts';
-export * from './line-heights';
-export * from './font-weights';
-
+export * from './font-sizes'
+export * from './font-weights'
+export * from './fonts'
+export * from './line-heights'
+export * from './font-weights'
 
 export * from './radii'
 export * from './space'
-
-
